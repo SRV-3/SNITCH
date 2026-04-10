6303bcbba1d9e8e472aaf86e3158c511a0e23363
Sourav <thakursourav778@gmail.com>
2026-04-10 12:37:12 +0530

    Add validation logic for user registration

---
+++ backend/src/vaildator/auth.validator.js
@@ -0,0 +1,29 @@
+import { body, validationResult } from "express-validator";
+
+function validationRequest(req, res, next) {
+  const error = validationRequest(req);
+
+  if (!error.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+}
+
+export const validateRegisterUser = [
+  body("email").isEmail().withMessage("Enter a valid email"),
+  body("contact")
+    .notEmpty()
+    .withMessage("Contact is required")
+    .matches(/^\d{10}$/)
+    .withMessage("contact must conatin 10 degits"),
+  body("password")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters long"),
+  body("fullname")
+    .notEmpty()
+    .withMessage("Full name is required")
+    .isLength({ min: 3 })
+    .withMessage("Full name must be at least 3 characters long"),
+  body("isSeller").isBoolean().withMessage("isSeller must be a boolean value"),
+
+  validationRequest,
+];

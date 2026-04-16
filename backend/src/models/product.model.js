5f402dbe2d4b947a82df8e3e2703d2cfe85efb36
Sourav <thakursourav778@gmail.com>
2026-04-16 10:18:21 +0530

    feat: add product model with schema definition for title, description, seller, price, and images

---
+++ backend/src/models/product.model.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+import { string } from "zod";
+import { required } from "zod/mini";
+
+const productSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    seller: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+    price: {
+      amount: {
+        type: Number,
+        required: true,
+      },
+      currency: {
+        type: String,
+        enum: ["USD", "EUR", "GBP", "JPY", "INR"],
+        default: "INR",
+      },
+    },
+    Image: [
+      {
+        url: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+  },
+  { Timestamp: true },
+);
+
+const productModel = mongoose.createModel("products", productSchema);
+
+export default productModel;
